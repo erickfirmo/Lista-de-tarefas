89a195a6e63b734580f8701261c59585e6a200b1
erickfirmo <erickluques@gmail.com>
2018-04-26 23:23:50 -0300

    implementação sass

---
+++ views/partials/partials.js
@@ -0,0 +1,11 @@
+function includeTopbar() {
+   document.write('<div id="topbar" class="container-fluid"><div class="row"> <div class="col-md-12">TOPBAR</div></div></div>');
+}
+
+function includeFooter() {
+   document.write('<div id="footer" class="container-fluid"><div class="row"> <div class="col-md-12">FOOTER</div></div></div>');
+}
+
+function includeSidebar() {
+   document.write('<div id="sidebar"></div>');
+}
